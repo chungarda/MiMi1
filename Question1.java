2c3d6c860ae390fd205f3d83120b2a63d25174cd
chungarda <art83420@gmail.com>
2015-04-27 11:15:43 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -1,6 +1,9 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		System.out.println("Test");
-		System.out.println("A AM011811 蔡詠傑提交");
+		System.out.println("B AM012273 鍾沅達提交");
+		
+	
+		
 	}
 }
